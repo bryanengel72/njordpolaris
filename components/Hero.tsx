7ec385a963e2dae0d043fa2b0cb0223ff0de66db
Bryan Engel <bryanengel@hotmail.com>
2026-02-22 22:41:09 -0600

    Adjust hero image focus to bottom

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,7 +10,7 @@ const Hero: React.FC = () => {
           <img
             src="https://zgfooiszwnzzwerznwwy.supabase.co/storage/v1/object/public/website-assets/Metal%20on%20belt.jpeg"
             alt="Hero image showing metal on belt"
-            className="w-full h-[60vh] md:h-[70vh] lg:h-[80vh] block object-cover object-[center_60%]"
+            className="w-full h-[60vh] md:h-[70vh] lg:h-[80vh] block object-cover object-bottom"
             style={{ display: 'block' }}
           />
           <div className="absolute inset-0 bg-black/5 pointer-events-none"></div>
