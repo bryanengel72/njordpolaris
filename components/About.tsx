b39c796673b038c1208498a2d3639a81cc18aff4
Bryan Engel <bryanengel@hotmail.com>
2026-02-23 19:14:04 -0600

    Update manhattan image url

--- components/About.tsx
+++ components/About.tsx
@@ -9,7 +9,7 @@ const About: React.FC = () => {
         {/* Full-width Image */}
         <div className="w-full bg-white mb-6">
           <img
-            src="https://zgfooiszwnzzwerznwwy.supabase.co/storage/v1/object/public/website-assets/manhattan_np.jpeg"
+            src="https://pub-e275050976b44ce6a57233bbbfab851a.r2.dev/manhattan_np.jpeg"
             alt="Cityscape view of Manhattan"
             className="w-full h-auto rounded-sm shadow-sm"
           />
